d729ba9b2013d525059d94760ae7bcb7fc086f21
Farooque Azam <farooqueazam004@gmail.com>
2026-04-11 13:38:19 +0530

    revert next-themes to 0.4.6

--- apps/web/src/components/theme-provider.tsx
+++ apps/web/src/components/theme-provider.tsx
@@ -4,5 +4,11 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <React.Fragment>
+        {children}
+      </React.Fragment>
+    </NextThemesProvider>
+  );
 }
